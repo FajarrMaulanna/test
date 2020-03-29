ea03f4ff3c46005f9b44e50c267a36eb377a7fa3
FajarrMaulanna <fajarmaulanna07@gmail.com>
2020-03-29 07:42:17 +0700

    testing

---
+++ add_donatur.php
@@ -0,0 +1,285 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="icon" type="image/png" href="assets/img/favicon.ico">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<title>RS Sumbar for COVID-19</title>
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+
+    <!--  Light Bootstrap Table core CSS    -->
+    <link href="assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
+
+
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="assets/css/demo.css" rel="stylesheet" />
+
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="sidebar" data-color="blue" data-image="assets/img/sidebar-5.jpg">
+
+    <!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
+
+
+    	<div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="#" class="simple-text">
+                    Covid-19
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li class="active">
+                    <a href="index.php">
+                        <i class="pe-7s-note2"></i>
+                        <p>Table List</p>
+                    </a>
+                </li>
+                <li>
+            </ul>
+    	</div>
+    </div>
+
+    <div class="main-panel">
+		<nav class="navbar navbar-default navbar-fixed">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Table List</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-dashboard"></i>
+								<p class="hidden-lg hidden-md">Dashboard</p>
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="glyphicon glyphicon-envelope"></i>
+                                    <b class="caret hidden-sm hidden-xs"></b>
+                                    <span class="notification hidden-sm hidden-xs">5</span>
+									<p class="hidden-lg hidden-md">
+										5 Notifications
+										<b class="caret"></b>
+									</p>
+                              </a>
+                              <ul class="dropdown-menu">
+                                <li><a href="#">Notification 1</a></li>
+                                <li><a href="#">Notification 2</a></li>
+                                <li><a href="#">Notification 3</a></li>
+                                <li><a href="#">Notification 4</a></li>
+                                <li><a href="#">Another notification</a></li>
+                              </ul>
+                            </li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                           <a href="">
+                               <p>Account</p>
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <p>
+										Dropdown
+										<b class="caret"></b>
+									</p>
+
+                              </a>
+                              <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                              </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <p>Log out</p>
+                            </a>
+                        </li>
+						<li class="separator hidden-lg hidden-md"></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <?php
+            include "koneksi.php";
+            function input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+                $nama_rs=input($_POST["nama_rs"]);
+                $donatur=input($_POST["donatur"]);
+                $apd=input($_POST["apd"]);
+                $reagen=input($_POST["reagen"]);
+                $viral=input($_POST["viral"]);
+                $rapid=input($_POST["rapid"]);
+                $nasal=input($_POST["nasal"]);
+                $ventilator=input($_POST["ventilator"]);
+                $tanggal= date("Y-m-d H:i:s");
+                
+                $sql="insert into donatur (rs_id,nama_donatur,apd,reagen,viral,rapid,nasal,ventilator,created_at) values ('$nama_rs','$donatur','$apd','$reagen','$viral','$rapid','$nasal','$ventilator','$tanggal')";
+                $hasil=mysqli_query($kon,$sql);
+
+                if ($hasil) {
+                    echo "<div class='alert alert-success'> Data Berhasil disimpan.</div>";
+                }
+                else {
+                    echo "<div class='alert alert-danger'> Data Gagal disimpan.</div>";
+
+                }
+            }
+        ?> 
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Tabel Induk</h4>
+                                <p class="category">Data seluruh item Rumah Sakit Sumbar</p>
+                            </div>
+                            <div class="container-sm border">
+                                <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+                                    <div class="form-group">
+                                        <select class="form-control" name="nama_rs">
+                                        <?php
+                                            include "koneksi.php"; 
+                                            $sql="select * from rs order by id asc";
+                                            $hasil=mysqli_query($kon,$sql);
+                                        ?>
+                                            <option>--Pilih Rumah Sakit--</option>
+                                            <?php if(mysqli_num_rows($hasil) > 0) { ?>
+                                                <?php while($row = mysqli_fetch_array($hasil)) { ?>
+                                                    <option><?php echo $row['id']?></option>
+                                                <?php }?>
+                                            <?php }?>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <p class="category">Nama Donatur</p>
+	                                    <input type="text" class="form-control" name="donatur">
+                                    </div>
+                                    <div class="form-group">
+                                        <p class="category">APD</p>
+	                                    <input type="number" min="0" class="form-control" name="apd">
+                                    </div>
+                                    <div class="form-group">
+                                        <p class="category">Reagen RTPCR</p>
+	                                    <input type="number" min="0" class="form-control" name="reagen">
+                                    </div>
+                                    <div class="form-group">
+                                        <p class="category">Viral Transfer Media</p>
+	                                    <input type="number" min="0" class="form-control" name="viral">
+                                    </div>
+                                    <div class="form-group">
+                                        <p class="category">Rapid Diagnostic Test</p>
+	                                    <input type="number" min="0" class="form-control" name="rapid">
+                                    </div>
+                                    <div class="form-group">
+                                        <p class="category">Nasal Swab</p>
+	                                    <input type="number" min="0" class="form-control" name="nasal">
+                                    </div>
+                                    <div class="form-group">
+                                        <p class="category">Ventilator</p>
+	                                    <input type="number" min="0" class="form-control" name="ventilator">
+                                    </div>
+                                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer class="footer">
+            <div class="container-fluid">
+                <nav class="pull-left">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Company
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Portfolio
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                               Blog
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+
+
+    </div>
+</div>
+
+
+</body>
+
+    <!--   Core JS Files   -->
+    <script src="assets/js/jquery.3.2.1.min.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+	<!--  Charts Plugin -->
+	<script src="assets/js/chartist.min.js"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="assets/js/bootstrap-notify.js"></script>
+
+    <!--  Google Maps Plugin    -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+	<script src="assets/js/light-bootstrap-dashboard.js?v=1.4.0"></script>
+
+	<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+	<script src="assets/js/demo.js"></script>
+
+
+</html>
